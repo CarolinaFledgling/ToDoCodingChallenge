4cea4525c484bd4833dec0b7d9d6de58658ba5ea
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-11-24 15:10:46 +0100

    handling error in the form , when we have empty input, or we will have less than 3 letters

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -2,30 +2,55 @@ import React, { useState } from "react";
 import { Button } from "../UI/Button/Button";
 import { Card } from "../UI/Card/Card";
 import { Wrapper } from "../UI/Wrapper/Wrapper";
+import styled from "./AddTask.module.css";
 
 export const AddTask = () => {
   const [enteredTask, setEnteredTask] = useState("");
+  const [error, setError] = useState("");
 
   const addTaskHandler = (e) => {
     e.preventDefault();
-    console.log(enteredTask)
+
+    if (enteredTask.trim().length === 0 || enteredTask.trim().length <= 3) {
+      setError({
+        title: "We have a error:",
+        message: "Please enter task, more than 3 letters",
+      });
+      return;
+    }
+
+    console.log(enteredTask);
+    setEnteredTask("");
+    setError("");
   };
 
   const userTaskChangeHandler = (e) => {
     setEnteredTask(e.target.value);
-    console.log("task:", enteredTask)
+    console.log("task:", enteredTask);
   };
   return (
     <Wrapper>
       <Card>
-        <form onSubmit={addTaskHandler} autoComplete="off">
-          <label htmlFor="task">Task name:</label>
+        <form
+          className={styled.form}
+          onSubmit={addTaskHandler}
+          autoComplete="off"
+        >
+          <label className={styled.label} htmlFor="task">
+            Task name:
+          </label>
           <input
+            className={styled.input}
             id="task"
             type="text"
             value={enteredTask}
             onChange={userTaskChangeHandler}
           />
+          {error && (
+            <p className={styled.error}>
+              {error.title} {error.message}
+            </p>
+          )}
           <Button type="submit">Add Task</Button>
         </form>
       </Card>

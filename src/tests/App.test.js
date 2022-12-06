89653817a462c3943c9853de378eba01ada709a0
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-12-06 10:31:37 +0100

    Added a test in Jest for checking if the functionality for adding a new task to the list works okay

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,19 +1,40 @@
-import { render, screen,fireEvent } from '@testing-library/react';
-import App from '../App';
-import { AddTask } from '../components/AddTask/AddTask';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "../App";
+import { AddTask } from "../components/AddTask/AddTask";
 
-test('renders the landing page', () => {
+import { reducerFn } from "../utility/reducer";
+import { v4 as uuidv4 } from "uuid";
+
+test("renders the landing page", () => {
   render(<App />);
 });
 
 test("AddTask renders without crashing", () => {
-  render(<AddTask />)
+  render(<AddTask />);
 });
-
 
 test("Form submission should not call add method if input field is empty", () => {
-  render(<AddTask  />);
+  render(<AddTask />);
   const btn = screen.getByText(/Add/i);
-  fireEvent.click(btn); 
+  fireEvent.click(btn);
 });
 
+test("adds a new task to the taskList", () => {
+  const initialState = {
+    taskList: [],
+  };
+
+  // dispatch an action to add a new task
+
+  const action = {
+    type: "NEW_TASK",
+    name: "test task",
+  };
+
+  const result = reducerFn(initialState, action);
+  console.log("result", result);
+
+  expect(result.taskList).toHaveLength(1);
+  expect(result.taskList[0].name).toEqual("test task");
+  expect(result.taskList[0].id).toBeDefined();
+});
